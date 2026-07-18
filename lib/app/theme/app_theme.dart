bd9116e8bf11920779b4210275d2278d8bb832d9
Erhan Ant <erhant.ant@gmail.com>
2026-07-18 18:05:03 +0300

    feat: integrate typography into app theme

--- lib/app/theme/app_theme.dart
+++ lib/app/theme/app_theme.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 
 import 'app_colors.dart';
+import 'app_typography.dart';
 
 class AppTheme {
   AppTheme._();
 
-  static ThemeData lightTheme = ThemeData(
+  static final ThemeData lightTheme = ThemeData(
     useMaterial3: true,
 
     colorScheme: ColorScheme.fromSeed(
@@ -14,5 +15,16 @@ class AppTheme {
     ),
 
     scaffoldBackgroundColor: AppColors.background,
+
+    textTheme: const TextTheme(
+      displayLarge: AppTypography.displayLarge,
+      headlineLarge: AppTypography.headlineLarge,
+      headlineMedium: AppTypography.headlineMedium,
+      titleLarge: AppTypography.titleLarge,
+      bodyLarge: AppTypography.bodyLarge,
+      bodyMedium: AppTypography.bodyMedium,
+      bodySmall: AppTypography.bodySmall,
+      labelLarge: AppTypography.labelLarge,
+    ),
   );
 }
