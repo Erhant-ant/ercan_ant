e955845ea4de68c92f88765ebd6b9f37449b8065
Erhan Ant <erhant.ant@gmail.com>
2026-07-18 17:45:02 +0300

    feat: add typography system

---
+++ lib/app/theme/app_typography.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class AppTypography {
+  AppTypography._();
+
+  static const TextStyle displayLarge = TextStyle(
+    fontSize: 56,
+    fontWeight: FontWeight.bold,
+    letterSpacing: -1.5,
+  );
+
+  static const TextStyle headlineLarge = TextStyle(
+    fontSize: 40,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle headlineMedium = TextStyle(
+    fontSize: 32,
+    fontWeight: FontWeight.w700,
+  );
+
+  static const TextStyle titleLarge = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle bodyLarge = TextStyle(
+    fontSize: 18,
+    height: 1.7,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle bodyMedium = TextStyle(
+    fontSize: 16,
+    height: 1.6,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle bodySmall = TextStyle(
+    fontSize: 14,
+    height: 1.5,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle labelLarge = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+  );
+}
